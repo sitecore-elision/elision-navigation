bdb4eb12e7fe7e3f4c93159a43bb91b3e9848b61
Jeffrey <jrondeau@degdigital.com>
2017-02-15 12:50:08 -0600

    Register dependencies

---
+++ src/Elision.Feature.Template.Navigation/App_Start/RegisterDependencies.cs
@@ -0,0 +1,15 @@
+﻿using Elision.Feature.Template.Navigation.SC.Integration;
+using Elision.Feature.Template.Navigation.SC.Integration.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+
+namespace Elision.Feature.Template.Navigation
+{
+    public class RegisterDependencies : IServicesConfigurator
+    {
+        public void Configure(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient<INavigationRepository<NavigationItem>, NavigationRepository<NavigationItem>>();
+        }
+    }
+}
